ffd1a2e5b80c3b09f2bc42608fd719270a1e399a
Steven Belbin <steven.belbin@outlook.com>
2016-03-22 21:12:28 -0400

    Added a placeholder parameter option to further illustrate the example.

--- deferred_function_call/Source.cpp
+++ deferred_function_call/Source.cpp
@@ -2,29 +2,31 @@
 #include <deque>
 #include <iostream>
 
-typedef std::function<void()> func_t;
+typedef std::function<void(int)> func_t;
 typedef std::deque<func_t> func_queue_t;
 
-void foo()
+void foo(int x)
 {
-   std::cout << "foo()" << std::endl;
+   std::cout << "foo(" << x << ")" << std::endl;
 }
 
-void bar(int value)
+void bar(int x, int y)
 {
-   std::cout << "bar(" << value << ")" << std::endl;
+   std::cout << "bar(" << x << ", " << y << ")" << std::endl;
 }
 
 int main(int, char*[])
 {
+   using namespace std::placeholders;
+
    func_queue_t queue;
 
-   queue.push_back(std::bind(&foo));
-   queue.push_back(std::bind(&bar, 10));
+   queue.push_back(std::bind(&foo, _1));
+   queue.push_back(std::bind(&bar, _1, 10));
 
    for (const auto& func : queue)
    {
-      func();
+      func(5);
    }
 
    return 0;
